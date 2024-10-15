a92a52c83173cfa80f90eb91716ee96c15a95768
prasadpatankar <69724452+prasadpatankar@users.noreply.github.com>
2024-10-15 11:16:00 +0530

    Add files via upload

--- pages/ChatGPT.py
+++ pages/ChatGPT.py
@@ -1,11 +1,16 @@
-import os
-import pandas as pd
+## Chatbot Code
 import google.generativeai as genai
 import streamlit as st
+import os
 
 # Configure Gemini API key
 my_api_key = "AIzaSyDE7hhHqhn_0KgVzJxh9nyhmlhjZVSuCOA"
 genai.configure(api_key=my_api_key)#genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    st.write(m.name)
+
 
 def get_gemini_response(question: str) -> str:
     """
@@ -21,20 +26,27 @@ def get_gemini_response(question: str) -> str:
     # Initialize the Gemini model
     model = genai.GenerativeModel('gemini-1.5-flash')
 
+    response = model.generate_content("What is the future of AI in one sentence?")
+    st.write(response.text)
+
     # Generate content based on the prompt and question
     response = model.generate_content(question)
 
     # Clean up and return the response
     return response.text.strip()
 
-st.title("Ask GPT")
+## Streamlit App
+#st.set_page_config(page_title="Basic Database Chatbot")
+#st.header("Basic Database Chatbot (using Gemini)")
 
-# Example prompt
-user_prompt = input("Enter your query: ")
+question = st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
 
-# Get response from Gemini API
-response = get_gemini_response(user_prompt)
-
-# Display the response
-print("Response from Gemini API:")
-print(response)
+# if submit is clicked
+if submit:
+    response = get_gemini_response(question)
+    # Clean the SQL query to remove any unwanted characters
+    cleaned_response = response.replace('```', '').strip()  # Remove any code block formatting
+    
+    st.subheader("The Gemini Response is")
+    st.write(cleaned_response)  # Display the SQL result DataFrame
