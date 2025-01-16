bce7d2f9fff5c957ad5d597eeaa5aa5be961bcb8
prasadpatankar <69724452+prasadpatankar@users.noreply.github.com>
2025-01-16 17:17:47 +0530

    Update mydashboard.py

--- pages/mydashboard.py
+++ pages/mydashboard.py
@@ -494,9 +494,7 @@ st.write(df)
 #df = pd.read_csv(file_path_mf2).iloc[-12:]
 df['Net_AUM'] = df['Net_AUM'].astype(str).str.replace(',', '', regex=False).apply(pd.to_numeric,errors='coerce')/ 1e5
 df['No_Folios'] = df['No_Folios'].astype(str).str.replace(',', '', regex=False).apply(pd.to_numeric,errors='coerce')/ 1e7
-st.write(df['Month'][-1])
-st.write(df['Month'].dtypes)
-df['Month'] = df['Month'].dt.strftime("%b %Y")
+df['Month'] = pd.to_datetime(df['Month']).dt.strftime("%b %Y")
 
 # Visualization 1: Bar Chart for Number of Orders by Quarter and Type
 with col8:
